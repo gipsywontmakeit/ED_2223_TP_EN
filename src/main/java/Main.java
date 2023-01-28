65533fcddf3f79a596d48f00a97529b3ee4e4a78
gipsywontmakeit <8200689@estg.ipp.pt>
2023-01-28 16:30:02 +0000

    Ownership done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-          JsonFile json = new JsonFile() ;
+        JsonFile json = new JsonFile() ;
 
         System.out.println("Hello World!");
 
